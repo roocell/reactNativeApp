12d7f4c56519d8153d562dba71cf9cfd165f296d
Michael Russell <roocell@gmail.com>
2017-10-19 15:14:06 -0400

    android support. moved main app code to a common file

---
+++ App.js
@@ -0,0 +1,59 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+
+//https://egghead.io/lessons/javascript-redux-generating-containers-with-connect-from-react-redux-visibletodolist
+import { Provider } from 'react-redux' // allows implicit passing of redux store via react context (video 25/26)
+
+import AppWithNavigationContainer from './navigation/AppNavigator';
+import configureStore from './reducers/configureStore'
+import { API_URL } from './config.js'
+
+export default class App extends Component {
+
+  store = configureStore();
+
+state = {
+    markers: [],
+
+    }
+
+
+
+    componentDidMount() {
+        console.log("compMount");
+
+        fetch(API_URL+'/markes')
+        .then( res => res.json() )
+        .then (function (res) {
+               console.log(res);
+               return res; // pass it onto the next .then
+               })
+        .then( markers => this.setState({markers}))
+        .catch(function (err) {
+               console.log(err);
+               return err;
+               });
+
+    }
+
+  render() {
+      console.log("rendering...");
+      // using the react-redux Provider component
+      // this provides implicit passing of redux store via react context
+      // so we don't have to pass the store down as a prop
+      return (
+          <Provider store = {this.store}>
+            <AppWithNavigationContainer markers = {this.state.markers} />
+          </Provider>
+      );
+  }
+
+}
